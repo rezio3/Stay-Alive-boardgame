60a43606e4ffa4374b7779079b76cfbd667d8f91
Joszko <jakub.rezler96@gmail.com>
2022-12-04 04:02:28 +0100

    modify copyright span

--- react-version/src/components/game/Copyright.js
+++ react-version/src/components/game/Copyright.js
@@ -3,13 +3,6 @@ import React from "react";
 const Copyright = () => {
 	return (
 		<footer className="copyright-container">
-			<span className="alert-reso">
-				If your game crashed, you've probably changed your window size
-			</span>
-			<span className="alert-reso">
-				DO NOT CHANGE YOUR SCREEN RESOLUTION DURING THE GAME! Adjust it before
-				you start.
-			</span>
 			<span className="copyright-text">
 				2022 Jakub Rezler || &copy; All rights reserved
 			</span>

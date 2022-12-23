608366bbafb911aa1951760df89ea2fa5c67f5fe
Joszko <jakub.rezler96@gmail.com>
2022-12-23 01:24:25 +0100

    move movement function to separate file - Character-movement.js

--- react-version/src/components/game/buttons/BoardFields.js
+++ react-version/src/components/game/buttons/BoardFields.js
@@ -1,71 +1,26 @@
 import React, { useContext } from "react";
 import { BoardContext } from "../../context/BoardContext";
 import { CharacterContext } from "../../context/CharContext";
-import Character from "../SelectedCharacterMove";
+import Character from "../Character";
+import { checkDistance } from "../functions/Character-movement";
 
 const BoardFields = (props) => {
 	const [board, setBoard] = useContext(BoardContext);
 	const [char, setChar] = useContext(CharacterContext);
-	const handleBoardBtn = (e) => {
-		if (board.btnId !== e.target.id) {
-			if (char.energy > 0) {
-				setBoard({
-					...board,
-					btnId: e.target.id,
-					// btnData: e.target,
-					prevRow: e.target.getAttribute("row"),
-					prevColumn: e.target.getAttribute("column"),
-				});
-			} else if (char.energy === 0) {
-				console.log("Za mało energii");
-			}
+
+	const handleFieldClick = (e) => {
+		const moveApproved = checkDistance(e, board, char);
+		if (moveApproved) {
+			setBoard({
+				...board,
+				btnId: e.target.id,
+				// btnData: e.target,
+				prevRow: e.target.getAttribute("row"),
+				prevColumn: e.target.getAttribute("column"),
+			});
 		}
 	};
-	const checkDistance = (e) => {
-		// approve movement if distance is correct (maximum one hex)
-		const { prevRow, prevColumn } = board;
-		let clickedRowNum = Number(e.target.getAttribute("row"));
-		let prevRowNum = Number(prevRow);
-		let clickedColumnNum = Number(e.target.getAttribute("column"));
-		let prevColumnNum = Number(prevColumn);
-		if (prevRow % 2 === 1) {
-			if (
-				(prevRowNum === clickedRowNum + 1 &&
-					prevColumnNum === clickedColumnNum + 1) ||
-				(prevRowNum === clickedRowNum + 1 &&
-					prevColumnNum === clickedColumnNum) ||
-				(prevRowNum + 1 === clickedRowNum &&
-					prevColumnNum === clickedColumnNum + 1) ||
-				(prevRowNum + 1 === clickedRowNum &&
-					prevColumnNum === clickedColumnNum) ||
-				(prevRowNum === clickedRowNum &&
-					prevColumnNum === clickedColumnNum + 1) ||
-				(prevRowNum === clickedRowNum && prevColumnNum + 1 === clickedColumnNum)
-			) {
-				handleBoardBtn(e);
-			} else {
-				console.log("Za daleko! Stoisz na rzędzie nieparzystym");
-			}
-		} else if (prevRow % 2 === 0) {
-			if (
-				(prevRowNum === clickedRowNum + 1 &&
-					prevColumnNum + 1 === clickedColumnNum) ||
-				(prevRowNum === clickedRowNum + 1 &&
-					prevColumnNum === clickedColumnNum) ||
-				(prevRowNum + 1 === clickedRowNum &&
-					prevColumnNum === clickedColumnNum) ||
-				(prevRowNum + 1 === clickedRowNum &&
-					prevColumnNum + 1 === clickedColumnNum) ||
-				(prevRowNum === clickedRowNum &&
-					prevColumnNum === clickedColumnNum + 1) ||
-				(prevRowNum === clickedRowNum && prevColumnNum + 1 === clickedColumnNum)
-			) {
-				handleBoardBtn(e);
-			} else {
-				console.log("Za daleko! Stoisz na rzędzie parzystym");
-			}
-		}
-	};
+
 	return (
 		<button
 			row={props.row}
@@ -85,7 +40,7 @@ const BoardFields = (props) => {
 			onClick={
 				props.id === 11 && board.charSetOnBoard === false
 					? props.dataArrowBtnHandler
-					: checkDistance
+					: handleFieldClick
 			}
 		>
 			{Number(board.btnId) === props.id ? <Character /> : null}
---
+++ react-version/src/components/game/functions/Character-movement.js
@@ -0,0 +1,63 @@
+export const checkDistance = (e, board, char) => {
+	// approve movement if distance is correct (maximum one hex)
+	const { prevRow, prevColumn } = board;
+	let clickedRowNum = Number(e.target.getAttribute("row"));
+	let prevRowNum = Number(prevRow);
+	let clickedColumnNum = Number(e.target.getAttribute("column"));
+	let prevColumnNum = Number(prevColumn);
+	console.log(e.target.id);
+	console.log(board.btnId);
+	if (board.btnId === e.target.id || e.target.id === "selected-character") {
+		console.log("Stoisz w miejscu");
+	} else if (prevRow % 2 === 1) {
+		if (
+			(prevRowNum === clickedRowNum + 1 &&
+				prevColumnNum === clickedColumnNum + 1) ||
+			(prevRowNum === clickedRowNum + 1 &&
+				prevColumnNum === clickedColumnNum) ||
+			(prevRowNum + 1 === clickedRowNum &&
+				prevColumnNum === clickedColumnNum + 1) ||
+			(prevRowNum + 1 === clickedRowNum &&
+				prevColumnNum === clickedColumnNum) ||
+			(prevRowNum === clickedRowNum &&
+				prevColumnNum === clickedColumnNum + 1) ||
+			(prevRowNum === clickedRowNum && prevColumnNum + 1 === clickedColumnNum)
+		) {
+			if (board.btnId !== e.target.id) {
+				if (char.energy > 0) {
+					return true;
+				} else if (char.energy === 0) {
+					console.log("Za mało energii");
+					return false;
+				}
+			}
+		} else {
+			console.log("Za daleko! Stoisz na rzędzie nieparzystym");
+			return false;
+		}
+	} else if (prevRow % 2 === 0) {
+		if (
+			(prevRowNum === clickedRowNum + 1 &&
+				prevColumnNum + 1 === clickedColumnNum) ||
+			(prevRowNum === clickedRowNum + 1 &&
+				prevColumnNum === clickedColumnNum) ||
+			(prevRowNum + 1 === clickedRowNum &&
+				prevColumnNum === clickedColumnNum) ||
+			(prevRowNum + 1 === clickedRowNum &&
+				prevColumnNum + 1 === clickedColumnNum) ||
+			(prevRowNum === clickedRowNum &&
+				prevColumnNum === clickedColumnNum + 1) ||
+			(prevRowNum === clickedRowNum && prevColumnNum + 1 === clickedColumnNum)
+		) {
+			if (char.energy > 0) {
+				return true;
+			} else if (char.energy === 0) {
+				console.log("Za mało energii");
+				return false;
+			}
+		} else {
+			console.log("Za daleko! Stoisz na rzędzie parzystym");
+			return false;
+		}
+	}
+};

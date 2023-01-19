e63c855fcbd382acea47c0f0037a8cf0ef217af4
Joszko <jakub.rezler96@gmail.com>
2023-01-19 02:43:54 +0100

    add LeftButtons component to LeftContent.js

--- react-version/src/components/game/stats-and-items/LeftContent.js
+++ react-version/src/components/game/stats-and-items/LeftContent.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { CharacterContext } from "../../context/CharContext";
 import Statistics from "./Statistics";
 import Items from "./Items";
+import LeftButtons from "./LeftButtons";
 
 const LeftContent = () => {
 	const { char, setChar } = useContext(CharacterContext);
@@ -13,6 +14,7 @@ const LeftContent = () => {
 			</header>
 			<Statistics />
 			<Items />
+			<LeftButtons />
 		</div>
 	);
 };

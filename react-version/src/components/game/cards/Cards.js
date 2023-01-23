9a4bad5f2851a922f1980635ab1e067f229b6374
Joszko <jakub.rezler96@gmail.com>
2023-01-23 02:30:02 +0100

    create Cards.js component

---
+++ react-version/src/components/game/cards/Cards.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FireCard from "./FireCard";
+import SejmitarCard from "./SejmitarCard";
+
+const Cards = () => {
+	return (
+		<>
+			<FireCard />
+			<SejmitarCard />
+		</>
+	);
+};
+
+export default Cards;

44832d4b8f5817c1825529ae61f8ad514c842c39
Joszko <jakub.rezler96@gmail.com>
2023-01-26 04:44:41 +0100

    add lose fight mechanics

--- react-version/src/components/game/functions/FightProcess.js
+++ react-version/src/components/game/functions/FightProcess.js
@@ -1,39 +1,61 @@
 export const fightProcess = (fight, setFight, char, setChar) => {
 	const number = Math.ceil(Math.random() * 6);
-	if (number >= 4) {
+	if (char.energy === 1 && number <= 3) {
 		setFight({
 			...fight,
-			fightWin: true,
+			fightEnd: true,
 			cubeRandomNumber: "",
 		});
 		setChar({
 			...char,
 			energy: char.energy - 1,
+			life: char.life - 6,
 		});
 		setTimeout(() => {
 			setFight({
 				...fight,
-				fightWin: true,
+				fightEnd: true,
+				addFrozenCrownAfterFight: false,
 				cubeRandomNumber: number,
 			});
 		}, 100);
 	} else {
-		setFight({
-			...fight,
-			fightWin: false,
-			cubeRandomNumber: "",
-		});
-		setChar({
-			...char,
-			energy: char.energy - 1,
-			life: char.life - 2,
-		});
-		setTimeout(() => {
+		if (number >= 4) {
 			setFight({
 				...fight,
-				fightWin: false,
-				cubeRandomNumber: number,
+				fightEnd: true,
+				cubeRandomNumber: "",
 			});
-		}, 100);
+			setChar({
+				...char,
+				energy: char.energy - 1,
+			});
+			setTimeout(() => {
+				setFight({
+					...fight,
+					fightEnd: true,
+					addFrozenCrownAfterFight: true,
+					cubeRandomNumber: number,
+				});
+			}, 100);
+		} else {
+			setFight({
+				...fight,
+				fightEnd: false,
+				cubeRandomNumber: "",
+			});
+			setChar({
+				...char,
+				energy: char.energy - 1,
+				life: char.life - 2,
+			});
+			setTimeout(() => {
+				setFight({
+					...fight,
+					fightEnd: false,
+					cubeRandomNumber: number,
+				});
+			}, 100);
+		}
 	}
 };

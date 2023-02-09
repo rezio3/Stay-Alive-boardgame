56af5f4225b488682be659bbee58e7b28a7cd5b1
Joszko <jakub.rezler96@gmail.com>
2023-02-09 22:23:29 +0100

    update StatsNotifications component

--- react-version/src/components/game/notifications/StatsNotifications.js
+++ react-version/src/components/game/notifications/StatsNotifications.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../../../style/css/StatsNotifications.css";
 
 const StatsNotifications = () => {
-	return <div className="stats-notifications-container"></div>;
+	return (
+		<div className="stats-notifications-container">
+			<span className="stats-notifications-span">Energia -1</span>
+		</div>
+	);
 };
 
 export default StatsNotifications;

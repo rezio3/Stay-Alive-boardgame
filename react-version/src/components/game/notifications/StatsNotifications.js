f87aec00564803ab616799df20810a8580990782
Joszko <jakub.rezler96@gmail.com>
2023-02-09 21:45:11 +0100

    add styles to StatsNotifications.js

--- react-version/src/components/game/notifications/StatsNotifications.js
+++ react-version/src/components/game/notifications/StatsNotifications.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "../../../style/css/StatsNotifications.css";
 
 const StatsNotifications = () => {
-	return <div></div>;
+	return <div className="stats-notifications-container"></div>;
 };
 
 export default StatsNotifications;

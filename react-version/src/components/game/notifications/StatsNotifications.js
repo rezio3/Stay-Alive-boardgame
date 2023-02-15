259c60eda220322a4d072beb70a9a281a33b486b
Joszko <jakub.rezler96@gmail.com>
2023-02-15 20:47:27 +0100

    rebuild whole StatsNotification.js component

--- react-version/src/components/game/notifications/StatsNotifications.js
+++ react-version/src/components/game/notifications/StatsNotifications.js
@@ -11,29 +11,122 @@ const StatsNotifications = () => {
 		const turnOffStatsAnimation = setTimeout(() => {
 			setStatsNote({
 				...statsNote,
+				life: false,
 				energy: false,
+				starvation: false,
+				sanity: false,
+				temperature: false,
 			});
 		}, 850);
 		return () => {
 			console.log("wyłączono timeout");
 			clearTimeout(turnOffStatsAnimation);
 		};
-	}, [statsNote.energy]);
-
+	}, [statsNote]);
+	let lifeNote;
 	let energyNote;
-	let sign;
-	const { energy, prevEnergy } = char;
+	let starvationNote;
+	let sanityNote;
+	let temperatureNote;
+	let lifeSign;
+	let energySign;
+	let starvationSign;
+	let sanitySign;
+	let temperatureSign;
+
+	const {
+		life,
+		prevLife,
+		energy,
+		prevEnergy,
+		starvation,
+		prevStarvation,
+		sanity,
+		prevSanity,
+		temperature,
+		prevTemperature,
+	} = char;
+
+	if (life !== prevLife) {
+		lifeNote = Math.abs(life - prevLife);
+		life > prevLife ? (lifeSign = "+") : (lifeSign = "-");
+	} else if (life === prevLife) {
+		lifeNote = false;
+	}
+
 	if (energy !== prevEnergy) {
 		energyNote = Math.abs(energy - prevEnergy);
-		energy > prevEnergy ? (sign = "+") : (sign = "-");
+		energy > prevEnergy ? (energySign = "+") : (energySign = "-");
+	} else if (energy === prevEnergy) {
+		energyNote = false;
 	}
+	if (starvation !== prevStarvation) {
+		starvationNote = Math.abs(starvation - prevStarvation);
+		starvation > prevStarvation
+			? (starvationSign = "+")
+			: (starvationSign = "-");
+	} else if (starvation === prevStarvation) {
+		starvationNote = false;
+	}
+	if (sanity !== prevSanity) {
+		sanityNote = Math.abs(sanity - prevSanity);
+		sanity > prevSanity ? (sanitySign = "+") : (sanitySign = "-");
+	} else if (sanity === prevSanity) {
+		sanityNote = false;
+	}
+	if (temperature !== prevTemperature) {
+		temperatureNote = Math.abs(temperature - prevTemperature);
+		temperature > prevTemperature
+			? (temperatureSign = "+")
+			: (temperatureSign = "-");
+	} else if (temperature === prevTemperature) {
+		temperatureNote = false;
+	}
+
+	let lifeOn = true;
+	if (life === prevLife) lifeOn = false;
+	let energyOn = true;
+	if (energy === prevEnergy) energyOn = false;
+	let starvationOn = true;
+	if (starvation === prevStarvation) starvationOn = false;
+	let sanityOn = true;
+	if (sanity === prevSanity) sanityOn = false;
+	let temperatureOn = true;
+	if (temperature === prevTemperature) temperatureOn = false;
 
 	return (
 		<div className="stats-notifications-container">
-			<span className="stats-notifications-span">
-				Energia {sign}
-				{energyNote}
-			</span>
+			{statsNote.life && lifeOn ? (
+				<span className="stats-notifications-span life">
+					Życie {lifeSign}
+					{lifeNote}
+				</span>
+			) : null}
+			{statsNote.energy && energyOn ? (
+				<span className="stats-notifications-span energy">
+					Energia {energySign}
+					{energyNote}
+				</span>
+			) : null}
+
+			{statsNote.starvation && starvationOn ? (
+				<span className="stats-notifications-span starvation">
+					Głód {starvationSign}
+					{starvationNote}
+				</span>
+			) : null}
+			{statsNote.sanity && sanityOn ? (
+				<span className="stats-notifications-span sanity">
+					Psychika {sanitySign}
+					{sanityNote}
+				</span>
+			) : null}
+			{statsNote.temperature && temperatureOn ? (
+				<span className="stats-notifications-span temp">
+					Temperatura {temperatureSign}
+					{temperatureNote}
+				</span>
+			) : null}
 		</div>
 	);
 };

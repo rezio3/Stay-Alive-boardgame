4f996ef107fdcfd9b828e7a6b02e72b6ac946df2
Joszko <jakub.rezler96@gmail.com>
2023-02-27 01:25:05 +0100

    create testing array for event1 in EventsArrayShuffle.js

--- react-version/src/components/game/functions/EventsArrayShuffle.js
+++ react-version/src/components/game/functions/EventsArrayShuffle.js
@@ -1,24 +1,28 @@
 export const arrayShuffle = (event, setEvent) => {
-	let i;
-	let eventArrayCount;
-	for (eventArrayCount = [], i = 0; i < 20; ++i) {
-		eventArrayCount[i] = i;
-	}
-	shuffle(eventArrayCount);
-	function shuffle(array) {
-		let tmp,
-			current,
-			top = array.length;
-		if (top)
-			while (--top) {
-				current = Math.floor(Math.random() * (top + 1));
-				tmp = array[current];
-				array[current] = array[top];
-				array[top] = tmp;
-			}
-		setEvent({
-			...event,
-			eventsArray: array,
-		});
-	}
+	// let i;
+	// let eventArrayCount;
+	// for (eventArrayCount = [], i = 0; i < 20; ++i) {
+	// 	eventArrayCount[i] = i;
+	// }
+	// shuffle(eventArrayCount);
+	// function shuffle(array) {
+	// 	let tmp,
+	// 		current,
+	// 		top = array.length;
+	// 	if (top)
+	// 		while (--top) {
+	// 			current = Math.floor(Math.random() * (top + 1));
+	// 			tmp = array[current];
+	// 			array[current] = array[top];
+	// 			array[top] = tmp;
+	// 		}
+	// 	setEvent({
+	// 		...event,
+	// 		eventsArray: array,
+	// 	});
+	// }
+	setEvent({
+		...event,
+		eventsArray: [19, 18, 17, 1],
+	});
 };

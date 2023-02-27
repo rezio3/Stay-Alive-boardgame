749881ccbd59fc211e1d0863e49f8e9f541bd195
Joszko <jakub.rezler96@gmail.com>
2023-02-27 01:41:08 +0100

    create condition in Event1.js

--- react-version/src/components/game/functions/EventsFunctions/Event1.js
+++ react-version/src/components/game/functions/EventsFunctions/Event1.js
@@ -1,3 +1,9 @@
-export const event1 = () => {
-	// console.log("event1 function");
+export const event1 = ({ rounds, char, board }) => {
+	console.log("event1 - złowroga ciemność");
+	if (
+		rounds.round % 2 === 1 &&
+		board.biomPlayerStandsOn !== "crater" &&
+		char.inventoryItems.torch === 0
+	) {
+	}
 };

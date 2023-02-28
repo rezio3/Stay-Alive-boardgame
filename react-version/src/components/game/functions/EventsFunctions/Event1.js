203b133421ec6fe65409e3c5f8115d5487b2694b
Joszko <jakub.rezler96@gmail.com>
2023-02-28 01:55:19 +0100

    create event1 mechanism in Event1.js

--- react-version/src/components/game/functions/EventsFunctions/Event1.js
+++ react-version/src/components/game/functions/EventsFunctions/Event1.js
@@ -1,9 +1,32 @@
-export const event1 = ({ rounds, char, board }) => {
+import { changeCharStats } from "../Stats-changer/ChangeCharStats";
+
+export const event1 = ({ rounds, char, setChar, board }) => {
 	console.log("event1 - złowroga ciemność");
 	if (
 		rounds.round % 2 === 1 &&
 		board.biomPlayerStandsOn !== "crater" &&
 		char.inventoryItems.torch === 0
 	) {
+		let sanitySubstractor;
+		switch (rounds.difficulty) {
+			case "easy":
+				sanitySubstractor = 1;
+				break;
+			case "medium":
+				sanitySubstractor = 2;
+				break;
+			case "hard":
+				sanitySubstractor = 3;
+				break;
+			default:
+				sanitySubstractor = 1;
+		}
+
+		changeCharStats({
+			char: char,
+			setChar: setChar,
+			sanity: char.sanity - sanitySubstractor,
+			event: "eventCard",
+		});
 	}
 };

7e57dc0d1bebdd609fae1ee50755c0764b33e590
Joszko <jakub.rezler96@gmail.com>
2023-03-02 01:38:48 +0100

    create basic event2 mechanics

--- react-version/src/components/game/functions/EventsFunctions/Event2.js
+++ react-version/src/components/game/functions/EventsFunctions/Event2.js
@@ -1,3 +1,35 @@
-export const event2 = () => {
+export const event2 = ({
+	rounds,
+	char,
+	setChar,
+	buttons,
+	setButtons,
+	anim,
+	setAnim,
+	fight,
+	setFight,
+}) => {
 	console.log("event2 function");
+	if (rounds.round % 2 === 1) {
+		setButtons({
+			...buttons,
+			useHexButton: false,
+			endTurnButton: false,
+		});
+		setChar({
+			...char,
+			cantMove: true,
+		});
+		setTimeout(() => {
+			setFight({
+				...fight,
+				fightActive: true,
+				monsterName: "",
+			});
+			setAnim({
+				...anim,
+				fight: true,
+			});
+		}, 1500);
+	}
 };

d8bb77931eaeb0fac800840a750b290cae06be43
Joszko <jakub.rezler96@gmail.com>
2023-03-09 02:08:24 +0100

    add condition to Event3.js

--- react-version/src/components/game/functions/EventsFunctions/Event3.js
+++ react-version/src/components/game/functions/EventsFunctions/Event3.js
@@ -1,1 +1,10 @@
-export const event3 = ({ char, setChar, board }) => {};
+export const event3 = ({ char, setChar, board }) => {
+	const { resourcePlayerStandsOn, biomPlayerStandsOn } = board;
+	if (
+		resourcePlayerStandsOn !== tent &&
+		resourcePlayerStandsOn !== cave &&
+		biomPlayerStandsOn !== desert &&
+		biomPlayerStandsOn !== crater
+	) {
+	}
+};

832c81a407cd0ad659a5576f8bd1b73d7f292b9e
Joszko <jakub.rezler96@gmail.com>
2023-03-13 21:22:45 +0100

    create Event4 mechanics

--- react-version/src/components/game/functions/EventsFunctions/Event4.js
+++ react-version/src/components/game/functions/EventsFunctions/Event4.js
@@ -1,3 +1,29 @@
-export const event4 = () => {
-	console.log("event4 function");
+import { changeCharStats } from "../Stats-changer/ChangeCharStats";
+
+export const event4 = ({ char, setChar, rounds, statsNote, setStatsNote }) => {
+	// console.log("event4 - trujące powietrze");
+	let lifeSubstractor;
+	switch (rounds.difficulty) {
+		case "easy":
+			lifeSubstractor = 2;
+			break;
+		case "medium":
+			lifeSubstractor = 3;
+			break;
+		case "hard":
+			lifeSubstractor = 4;
+			break;
+		default:
+			lifeSubstractor = 2;
+	}
+	setTimeout(() => {
+		changeCharStats({
+			char: char,
+			setChar: setChar,
+			life: char.life - lifeSubstractor,
+			statsNote: statsNote,
+			setStatsNote: setStatsNote,
+			event: "eventCard",
+		});
+	}, 1800);
 };

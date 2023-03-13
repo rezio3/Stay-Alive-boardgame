5f42114240239bd42e029dc539f00c07474fe455
Joszko <jakub.rezler96@gmail.com>
2023-03-13 22:00:23 +0100

    add Event5 basic mechanics

--- react-version/src/components/game/functions/EventsFunctions/Event5.js
+++ react-version/src/components/game/functions/EventsFunctions/Event5.js
@@ -1,3 +1,44 @@
-export const event5 = () => {
-	console.log("event5 - upiorognom");
+export const event5 = (
+	char,
+	setChar,
+	rounds,
+	setRounds,
+	fight,
+	setFight,
+	buttons,
+	setButtons,
+	statsNote,
+	setStatsNote
+) => {
+	// console.log("event5 - upiorognom");
+	let time = rounds.round % 2 === 0 ? 500 : 1800;
+	if (rounds.round % 2 === 1) {
+		setButtons({
+			...buttons,
+			useHexButton: false,
+			endTurnButton: false,
+		});
+		setChar({
+			...char,
+			cantMove: true,
+		});
+		setTimeout(() => {
+			setFight({
+				...fight,
+				fightActive: true,
+				monsterName: "Wielkie Harpioperze",
+			});
+			setAnim({
+				...anim,
+				fight: true,
+			});
+		}, 1500);
+	} else {
+		setTimeout(() => {
+			setButtons({
+				...buttons,
+				endTurnButton: true,
+			});
+		}, time);
+	}
 };
